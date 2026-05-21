64caa9190865bd7eb6afa7c50b06ab9db619a2b4
huangchuancong <huangchuancong@qq.com>
2026-05-21 10:06:06 +0800

    feat: 路由改为hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import 'react-vant/lib/index.css';
 import './styles.css';
 import { ChurujingPage } from './pages/churujing/ChurujingPage';
@@ -9,14 +9,14 @@ import { KrakenPage } from './pages/kraken/KrakenPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/churujing" replace />} />
         <Route path="/churujing" element={<ChurujingPage />} />
         <Route path="/kraken" element={<KrakenPage />} />
         <Route path="/kraken/activity/:activityId" element={<KrakenActivityDetailPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
